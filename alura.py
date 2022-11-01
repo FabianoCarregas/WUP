2a83c3281ea5d7e7dd9a12fce685194ad80d0f8a
FabianoCarregas <fabianofac_@hotmail.com>
2022-11-01 13:47:06 -0300

    course issue

--- alura.py
+++ alura.py
@@ -17,25 +17,6 @@ def log_in(email, password, url):
     driver.find_element('xpath', '//*[@id="password"]').send_keys(pass_word)
     driver.find_element('xpath', '/html/body/div[3]/section/section[1]/form/div[1]/button').click()
 
-    # starting course
-    action = ActionChains(driver)
-    driver.find_element(By.CLASS_NAME, 'headerBusca-icon-svg').click()
-    driver.find_element(By.ID, 'headerBusca-campoBusca').send_keys('Certificação Java SE 7 Programmer I')
-    driver.find_element(By.CLASS_NAME, 'headerBusca-submit').click()
-    time.sleep(2)
-    driver.find_element('xpath', '/html/body/div[4]/div[2]/section/ul/li[1]/a/div/p').click()
-    driver.find_element(By.CLASS_NAME, 'course-header-button').click()
-    time.sleep(2)
-    action.send_keys(Keys.SPACE).perform()
-    time.sleep(8)
-
-    driver.find_element(By.CLASS_NAME, 'task-actions-button-next').click()
-    driver.find_element(By.CLASS_NAME, 'task-actions-button-next').click()
-    driver.find_element(By.CLASS_NAME, 'task-actions-button-next').click()
-    driver.find_element(By.CLASS_NAME, 'task-actions-button-next').click()
-    driver.find_element(By.CLASS_NAME, 'task-actions-button-next').click()
-    time.sleep(2)
-
     # learning
     driver.get(e_url)
 
@@ -129,7 +110,28 @@ def log_in(email, password, url):
     driver.find_element('xpath', '/html/body/header[1]/div/nav/div[6]/a/span').click()
     driver.find_element(By.CLASS_NAME, 'header-item-link--vitrinedev').click()
 
+    # starting course
+    action = ActionChains(driver)
+    driver.find_element(By.CLASS_NAME, 'headerBusca-icon-svg').click()
+    driver.find_element(By.ID, 'headerBusca-campoBusca').send_keys('Curso PNL: comunicação efetiva para o dia a dia')
+    driver.find_element(By.CLASS_NAME, 'headerBusca-submit').click()
+    time.sleep(2)
+    driver.find_element('xpath', '/html/body/div[4]/div[2]/section/ul/li[1]/a/div/p').click()
+    driver.find_element(By.CLASS_NAME, 'course-header-button').click()
+    time.sleep(2)
+    driver.get('https://cursos.alura.com.br/course/pnl-comunicacao-efetiva/task/76672')
+    time.sleep(2)
+    action.send_keys(Keys.SPACE).perform()
+    time.sleep(8)
+
+    driver.find_element(By.CLASS_NAME, 'task-actions-button-next').click()
+    driver.find_element(By.CLASS_NAME, 'task-actions-button-next').click()
+    driver.find_element(By.CLASS_NAME, 'task-actions-button-next').click()
+    driver.find_element(By.CLASS_NAME, 'task-actions-button-next').click()
+    driver.find_element(By.CLASS_NAME, 'task-actions-button-next').click()
+    time.sleep(2)
+
     # finishing
     driver.get(e_url)
     driver.find_element(By.CLASS_NAME, 'headerBusca-icon-svg').click()
-    driver.find_element(By.ID, 'headerBusca-campoBusca').send_keys('>>>>>>>- Alura warmed up -<<<<<<<')
+    driver.find_element(By.ID, 'headerBusca-campoBusca').send_keys('>>>>>>>- ALURA WARMED UP -<<<<<<<  :)')
